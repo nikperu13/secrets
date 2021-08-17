548636081de2fc73620dfe614d7aa0e12c9f5a85
Nicolas Garcia Rosell <nicolasgarciarosell@nicolass-air.home>
2021-08-17 15:27:52 -0400

    Level 4- Salting and Hashing passwords

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
-// hashing package
-var md5 = require("md5")
+
+// salting & hashing package
+var bcrypt = require("bcrypt");
+
+// needed to specify number of saltrounds for "bcrypt"
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,28 +25,11 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, use
 
 // slightly different from before
 // this is now a object created with mongoose.Schema!
-// needed in order to use "encrypt" with it. i.e mongoose-encryption
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
 
-// Create a secret string and use userSchema.plugin() to use it
-// encryptedFields:["",""] allows us to pick and choose which fields/parameters
-// we want to be encrypted
-// when we .save() a new object it will encrypt it
-// when we .find() an object it will decrypt its 
-
-// Behind the scenes "encrypt" will encrypt the password field
-// using "secret" string
-// const secret = "Thisisourlittlesecret."
-// For more security we will use a .env file and write out "secret"
-// encryption key there. Then to use it here from the .env file we 
-// use process.env.SECRET 
-/// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
-// We will use "md5" package instead to hash our
-// password! (line 64)
 
 const User = mongoose.model("User", userSchema)
 
@@ -59,43 +45,47 @@ app.get("/register", function(req,res){
 })
 app.post("/register", function(req,res){
    
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    // Once user is created only then can they see 
-    // the secrets page
-    // if no error send user to secrets page
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }else{
-            console.log(err)
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+        // for the password use "hash" that's being returned by
+        // the bcrypt.hash anonymous function
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        // Once user is created only then can they see 
+        // the secrets page
+        // if no error send user to secrets page
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets");
+            }else{
+                console.log(err)
+            }
+        })
     })
+
+    
 })
 app.post("/login", function(req,res){
     const email = req.body.username
-    // when using md5 we need to compare the password
-    // in its hash form in order to check if they
-    // are the same!
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     // find requested user and check if the email and password
     // matches a current users credentials
     User.findOne({email: email},function(err,foundUser){
         if(!err){
             if(foundUser){
-                if(password === foundUser.password){
-                    res.render("secrets")
-                }
-                else{
-                    res.render("login")
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                    // bcrypt.compare will an err or result(boolean) if the password
+                    // matches after checking for salt rounds and hash
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                })
             }
         }else{
-            console.log(err)
+            console.log(err);
         }
     })
 })
