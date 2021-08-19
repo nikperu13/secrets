a83067cbc409761e99606ac65dadfe5967cc18c9
Nicolas Garcia Rosell <nicolasgarciarosell@nicolass-air.home>
2021-08-18 21:34:53 -0400

    Level 6 - Google Oauth

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const session = require("express-session");
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
 
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -41,18 +42,39 @@ mongoose.set("useCreateIndex",true);
 // needed when wanted to use plugins! i.e .plugin()
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
-})
+    password: String,
+    googleId: String
+});
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // NOTE: while running locally it the web app will
 // remember the user is logged in but once we
@@ -85,6 +107,17 @@ app.get("/logout", function(req,res){
     req.logout();
     res.redirect("/");
 })
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope:['profile']})
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
+  });
 
 app.post("/register", function(req,res){
     // using the passport-local-mongoose package
